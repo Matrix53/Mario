409069417f9e714fce8c9744efa20beda2569637
luxia <allxst@me.com>
2021-06-09 21:10:35 +0800

    coin from the box

---
+++ src/BoxCoin.java
@@ -0,0 +1,54 @@
+class BoxCoin {
+    private int x, y; // position
+    private Image[] image = new Image[4];
+    private int i = 0;
+    private int jumpHeight = 5;
+    private int jumping = 15;
+    private int falling = 0;
+
+    public BoxCoin(int x, int y) {
+        this.x = x;
+        this.y = y;
+        for (int i = 0; i < 4; i++)
+            image[i] = new Image("images/boxCoin/boxCoin" + i + ".png");
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Image getImage() {
+        return image[i / 3];
+    }
+
+    public void animate() {
+        /*
+        * the animation of the coin from the box
+        */
+        if (++i >= 12) i -= 12;
+
+        // one jump of the coin
+        if (jumping > 0) {
+            y -= jumpHeight;
+            jumping--;
+            if (jumping == 0)
+                falling = 15;
+        } else if (falling > 0) {
+            y += jumpHeight;
+            falling--;
+        } else // and then the coin vanish
+            x = -50;
+    }
+}
