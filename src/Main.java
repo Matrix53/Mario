d3d7b16795f01042ea195678c16f1fb37b69893b
Matrix53 <1079207272@qq.com>
2021-06-09 17:22:28 +0800

    chore: add some comments

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,19 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
+/**
+ * 继承JavaFX的Application类作为游戏的主类，
+ * 游戏将从这个类开始运行
+ * @author Matrix53
+ * @version 1.0
+ */
 public class Main extends Application{
+    /**
+     * 重写父类的start方法，用来进行游戏的初始化
+     * @param stage 游戏的唯一stage，所有游戏内容都将在这个stage上面
+     */
     @Override
     public void start(Stage stage){
         Canvas canvas =new Canvas(800,500);
@@ -15,9 +27,22 @@ public class Main extends Application{
         stage.setTitle("SuperMario");
         Image icon=new Image("images/icon/marioIcon.png");
         stage.getIcons().add(icon);
+
+        ArrayList<String> input=new ArrayList<>();
+        scene.setOnKeyPressed(event ->{
+            String keyCode=event.getCode().toString();
+            if(!input.contains(keyCode)){
+                input.add(keyCode);
+            }
+        });
+
         stage.show();
     }
 
+    /**
+     * 游戏主类的main方法，JavaFX程序的主入口
+     * @param args 传递给JavaFX程序的参数列表
+     */
     public static void main(String args[]){
         launch(args);
     }
