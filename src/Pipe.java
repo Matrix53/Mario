283390182c7964361be4ee3f9bfc6b9b4c801efb
Matrix53 <1079207272@qq.com>
2021-06-10 10:04:22 +0800

    add pipe class

---
+++ src/Pipe.java
@@ -0,0 +1,33 @@
+import javafx.scene.image.Image;
+
+public class Pipe implements Collidable{
+    private int x;
+    private int y;
+    private Image image;
+
+    public Pipe(int x,Image image){
+        this.image=image;
+        this.x=x;
+        this.y=(int)(550-85- image.getHeight());
+    }
+
+    @Override
+    public double getX(){
+        return x;
+    }
+
+    @Override
+    public double getY(){
+        return y;
+    }
+
+    @Override
+    public double getWidth(){
+        return image.getWidth();
+    }
+
+    @Override
+    public double getHeight(){
+        return image.getHeight();
+    }
+}
