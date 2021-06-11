bfc749306c6d6c4141b64307e5acfb11d84cb666
luxia <allxst@me.com>
2021-06-11 15:52:00 +0800

    level 1 little change

--- src/level/Level1.java
+++ src/level/Level1.java
@@ -1,24 +1,26 @@
 package level;
 
+import entity.Enemy;
 import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Level1 {
-    public static void start(Stage stage){
-        Controller controller=Controller.getInstance();
-        Scene scene= controller.getScene();
+    public static void start(Stage stage) {
+        Controller controller = Controller.getInstance();
+        Scene scene = controller.getScene();
         stage.setScene(scene);
         stage.setTitle("SuperMario");
-        Image icon=new Image("images/icon/marioIcon.png");
+        Image icon = new Image("images/icon/marioIcon.png");
 
         stage.getIcons().add(icon);
-        AnimationTimer timer=controller.getTimer();
+        AnimationTimer timer = controller.getTimer();
         timer.start();
 
-        controller.addSmallPipe(500);
-
+        controller.addSmallPipe(600);
+        controller.addEnemy(700, 550 - 85);
+        controller.addSmallPipe(800);
         stage.show();
     }
 }
