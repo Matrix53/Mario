8a0e1f3375a83e6c6153a3fd011e05327c915bf0
Matrix53 <1079207272@qq.com>
2021-06-12 11:27:59 +0800

    chore: add record class and modify main controller

--- src/entity/Record.java
+++ src/entity/Record.java
@@ -1,5 +1,21 @@
 package entity;
 
 public class Record {
+    private int score;
+    private int time;
+    private int level;
 
+    public Record(){
+        this.score=0;
+        this.time=0;
+        this.level=0;
+    }
+
+    public void readRecord(){
+
+    }
+
+    public void writeRecord(){
+
+    }
 }
